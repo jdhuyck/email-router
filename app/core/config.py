a3d08d9631ebc2e24bc88bdc6fdd80c8d213e149
Jody Huyck-Jones <68274088+jdhuyck@users.noreply.github.com>
2025-08-20 17:52:42 +0100

    Fix model config

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 class Settings(BaseSettings):
     """Application settings loaded from environment variables"""
     app_name: str = "AI Email Router API"
-    model_name: str = "facebook/bart-large/mnli"  # Zero-shot model
+    model_name: str = "facebook/bart-large-mnli"  # Zero-shot model
 
     class Config:
         env_file = ".env"
