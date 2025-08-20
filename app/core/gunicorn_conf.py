cf4375fccbc43a9153746611422e96c6b99d58cf
Jody Huyck-Jones <68274088+jdhuyck@users.noreply.github.com>
2025-08-20 21:02:55 +0100

    Fix gitignore

---
+++ app/core/gunicorn_conf.py
@@ -0,0 +1,15 @@
+import multiprocessing
+
+
+bind = "0.0.0.0:8000"
+
+
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+
+
+loglevel = "info"
+accesslog = "-"  # Log to stdout
+errorlog = "-"  # Log to stderr
+
+timeout = 120
