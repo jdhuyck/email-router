352b1d7031fa196e3203d189ce1d06f660f32e53
Jody Huyck-Jones <68274088+jdhuyck@users.noreply.github.com>
2025-08-20 17:41:40 +0100

    Create API request and response models

---
+++ app/core/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class ClassificationRequest(BaseModel):
+    """Request model for the classify endpoint."""
+    email_text: str
+
+
+class ClassificationResponse(BaseModel):
+    """Response model for the classify endpoint."""
+    sequence: str
+    labels: List[str]
+    scores: List[float]
